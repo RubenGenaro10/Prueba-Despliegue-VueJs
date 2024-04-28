f37a37f1f886514901958bebf47ce85c3b57d69d
RubenGenaro10 <rubengenaro10@gmail.com>
2024-04-28 08:24:19 -0500

    se agrego url corect

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://github.com/RubenGenaro10.github.io/Prueba-Despliegue-VueJs'
+  base:'https://github.com/rubengenaro10.github.io/Prueba-Despliegue-VueJs'
 })
